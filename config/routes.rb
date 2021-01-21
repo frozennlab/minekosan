0a4f16d03f8184725809a18fd0839446a33e307d
Paul AZEMA <rigonkmalk@gmail.com>
2021-01-21 09:28:00 +0100

    fix: code scanning

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
   root to: 'pages#index'
   get '/faq', to: 'pages#faq'
   get '/contacts', to: 'contacts#new'
-  resources "contacts", only: [:new, :create]
+  resources 'contacts', only: %i[new create]
 end
